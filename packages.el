a8abeb4a5126e02569ae5c547937b85383a3376b
cupkax <cupkax@protonmail.com>
2026-01-02 11:09:20 +0300

    Testing new config again

---
+++ packages.el
@@ -0,0 +1,83 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; General
+(package! auto-olivetti
+  :recipe (:host sourcehut :repo "ashton314/auto-olivetti"))
+
+;; LATEX
+(package! laas
+  :recipe (:host github :repo "tecosaur/LaTeX-auto-activating-snippets"))
+
+;; Icons
+(package! nerd-icons-ibuffer
+  :recipe (:host github :repo "seagle0128/nerd-icons-ibuffer"))
+
+;; LLM
+(package! inline-diff
+  :recipe (:repo "https://code.tecosaur.net/tec/inline-diff"))
+
+;; ORG-MODE
+;; Org-mode from tecosaur's development branch
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+         (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+         :files
+         (:defaults "etc")
+         :build t :pre-build
+         (with-temp-file "org-version.el"
+           (require 'lisp-mnt)
+           (let
+               ((version
+                 (with-temp-buffer
+                   (insert-file-contents "lisp/org.el")
+                   (lm-header "version")))
+                (git-version
+                 (string-trim
+                  (with-temp-buffer
+                    (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+                    (buffer-string)))))
+             (insert
+              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+              "(provide 'org-version)\n"))))
+  :pin nil)
+
+(unpin! org) ; there be bugs
+
+
+(package! org-super-agenda
+  :recipe (:host github :repo "alphapapa/org-super-agenda"))
+
+(package! org-contrib
+  :recipe (:host nil :repo "https://git.sr.ht/~bzg/org-contrib"
+           :files ("lisp/*.el"))
+  :pin "6422b265f1150204f024e33d54f2dcfd8323005c")
+
+(package! org-modern
+  :recipe (:host github :repo "minad/org-modern"))
+
+(package! svg-tag-mode
+  :recipe (:host github :repo "rougier/svg-tag-mode"))
+
+(package! org-appear
+  :recipe (:host github :repo "awth13/org-appear"))
+
+(package! org-roam-ui)
+
+(package! org-csl-activate
+  :recipe (:host github :repo "andras-simonyi/org-cite-csl-activate"))
+
+(package! org-special-block-extras
+  :recipe (:host github :repo "alhassy/org-special-block-extras"))
+
+(package! engrave-faces
+  :recipe (:host github :repo "tecosaur/engrave-faces"))
+
+;; RSS
+(package! elfeed-score
+  :recipe (:host github :repo "sp1ff/elfeed-score"))
+
+;; Magit
+(package! magit-file-icons
+  :recipe (:host github :repo "gekoke/magit-file-icons"))
